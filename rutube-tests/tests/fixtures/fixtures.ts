d2d3734238b5ee387c1b45026372f3dee246ea6a
Вадим В. <vadim-verlan@mail.ru>
2025-08-17 09:54:00 +0300

    Добавление фикстур

---
+++ rutube-tests/tests/fixtures/fixtures.ts
@@ -0,0 +1,21 @@
+import { test as base } from '@playwright/test';
+import { MainPage } from '../pages/MainPage';
+
+// Declare the types of your fixtures.
+type MyFixtures = {
+    mainPage: MainPage;
+};
+
+export const test = base.extend<MyFixtures>({
+  mainPage: async ({ page }, use) => {
+    // Set up the fixture.
+    const mainPage = new MainPage(page);
+    await mainPage.open();
+    await mainPage.closePopupUnauthotizaded();
+    await mainPage.closeCookiesAlert;
+    // Use the fixture value in the test.
+    await use(mainPage);
+
+  },
+});
+export { expect } from '@playwright/test';
--- rutube-tests/tests/specs/unauthorizaded/manePage.spec.ts
+++ rutube-tests/tests/specs/unauthorizaded/manePage.spec.ts
@@ -1,26 +1,14 @@
-import test from '@playwright/test';
+import {test, expect} from '../../fixtures/fixtures';
 import { MainPage } from '../../pages/MainPage';
 
-test('Проверка доступности элементов хедоров', async ({ page}) => {
-   const mainPage = new MainPage(page);
-   await mainPage.open();
-   await mainPage.closePopupUnauthotizaded();
-   await mainPage.closeCookiesAlert();
+test('Проверка доступности элементов хедоров', async ({ mainPage }) => {
    await mainPage.headeHasCorrectAriaSnapshot();
 });
 
-test('Проверка доступности табов категории', async ({ page}) => {
-   const mainPage = new MainPage(page);
-   await mainPage.open();
-   await mainPage.closePopupUnauthotizaded();
-   await mainPage.closeCookiesAlert();
+test('Проверка доступности табов категории', async ({ mainPage }) => {
    await mainPage.categoriesTabsCorrectAriaSnapshot();
 });
 
-test('Проверка доступности элементов бокового меню', async ({ page}) => {
-   const mainPage = new MainPage(page);
-   await mainPage.open();
-   await mainPage.closePopupUnauthotizaded();
-   await mainPage.closeCookiesAlert();
+test('Проверка доступности элементов бокового меню', async ({ mainPage }) => {
    await mainPage.menuCorrectAriaSnapshot();
 });
